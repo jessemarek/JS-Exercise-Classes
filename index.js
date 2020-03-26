2f01351056ca174740c7456f744f51a5eec3c42b
unknown <dethgrenade@gmail.com>
2020-03-26 11:53:57 -0700

    2nd task complete

--- index.js
+++ index.js
@@ -77,7 +77,31 @@ class Person {
 */
 
 class Car {
-
+  constructor(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+  
+  fill(gallons){
+    return this.tank += gallons;
+  }
+  
+  drive(distance){
+  
+    const driveableDist = this.tank * this.milesPerGallon;
+    
+    if(distance < driveableDist){
+      this.odometer += distance;
+      this.tank -= distance / this.milesPerGallon;
+    }
+    if(distance > driveableDist){
+      this.odometer += driveableDist;
+      this.tank -= driveableDist / this.milesPerGallon;
+      return `I ran out of fuel at ${driveableDist} miles!`
+    }
+  }
 }
 
 /*
